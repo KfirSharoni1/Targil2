6f752eb7d5b05f62c400b9e85ce7d47ea92618de
Kfir Sharoni <kfirsharoni1@gmail.com>
2024-01-24 10:57:11 +0200

    add java doc

--- Targil2/src/SharedData.java
+++ Targil2/src/SharedData.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
 
+/**
+ * @author kfirs
+ * 
+ *
+ */
 public class SharedData 
 {
+	/**
+	 * Class to share array within the threads
+	 */
 	private ArrayList<Integer> array;
 	private boolean [] winArray;
 	private boolean flag;
 	private final int b;
+	
+	/** 	Constractor that get Array list and integers from the user 
+	 * @param array     Array of integers
+	 * @param b			the size of the array
+	 */
 	
 	public SharedData(ArrayList<Integer> array, int b) {
 		
@@ -13,31 +26,49 @@ public class SharedData
 		this.b = b;
 	}
 
+	/** get function of win array
+	 * @return 	return the array of booleans
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
 	}
 
+	/** set function of win array
+	 * @param winArray			array of booleans
+	 */
 	public void setWinArray(boolean [] winArray) 
 	{
 		this.winArray = winArray;
 	}
 
+	/**get function of array of integers
+	 * @return return the array
+	 */
 	public ArrayList<Integer> getArray() 
 	{
 		return array;
 	}
 
+	/** get function of array size
+	 * @return  the size of array 
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/** get functoin for flag
+	 * @return 			booleans flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/** set function of flag
+	 * @param flag  boolean flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
